572bd4765778d4541a1b4318c96d10b25f5604df
Michael Warwick <michael.warwick@warrenandmahoney.com>
2023-05-23 10:46:36 +1200

    Crop now image capture Intent is to use for AI image generation

--- maw-dev.extension/pyMAW.tab/Test.panel/CropViews.pushbutton/script.py
+++ maw-dev.extension/pyMAW.tab/Test.panel/CropViews.pushbutton/script.py
@@ -1,57 +1,3 @@
-# Based on a node by Modelical
-# www.modelical.com
-
-import clr
+from pyrevit import DB, revitfrom pyrevit import forms, scriptfrom pyrevit.coreutils import appdatafrom System import Urifrom System.Windows.Media.Imaging import BitmapImageimport osimport sysdoc = revit.docvw = doc.ActiveViewlandscape = Trueif vw.CropBoxActive:    xyzMin = vw.CropBox.Min    xyzMax = vw.CropBox.Max    if xyzMax.X - xyzMin.X < xyzMax.Y - xyzMin.Y:        landscape = False    #print("View {} Width: {} Height {}".format(vw.Name, xyzMax.X - xyzMin.X, xyzMax.Y - xyzMin.Y))try:    errorReport = None    with revit.Transaction("View to image"):        options = DB.ImageExportOptions()        tmp_file = "C:/Temp/" + "Image_" + vw.Name        tmp_file = script.get_instance_data_file("Image_" + vw.Name, "jpg")        tmp_file = tmp_file[:-3] + "jpg"        #options.ShadowViewsFileType = "PNG",        #options.HLRandWFViewsFileType = DB.ImageFileType.PNG,        if options.IsValidFileName(tmp_file):            options.FilePath = tmp_file        else:            import time            options.FilePath = script.get_universal_data_file("Image_" + time.strftime("%Y%m%d-%H%M%S"), "jpg")        if vw.CropBoxActive:            options.ExportRange = DB.ExportRange.CurrentView #API seems to prohibit saving list of views to project as images        else:            options.ExportRange = DB.ExportRange.VisibleRegionOfCurrentView        options.ImageResolution = DB.ImageResolution.DPI_150        options.ZoomType = DB.ZoomFitType.FitToPage        if landscape:            options.FitDirection = DB.FitDirectionType.Vertical        else:            options.FitDirection = DB.FitDirectionType.Horizontal        options.PixelSize = 512        result = doc.ExportImage(options)        #Transaction endexcept:    # if error occurs anywhere in the process catch it    import traceback    errorReport = traceback.format_exc()if None != errorReport:    print(errorReport)
 
-#Load Revit API
-from pyrevit import revit, DB
-from pyrevit import forms
-from pyrevit import script
-
-logger = script.get_logger()
-
-doc = __revit__.ActiveUIDocument.Document
-
-
-def duplicableview(view):
-    return view.CanViewBeDuplicated(DB.ViewDuplicateOption.Duplicate)
-
-
-def dependant_views(vw, scopeboxlist):
-    with revit.Transaction('Duplicate selected views'):
-        dupop = DB.ViewDuplicateOption.AsDependent
-        count = 0
-        #for vw in viewlist:
-        crop = vw.GetCropRegionShapeManager()
-        if crop.ShapeSet:
-            crop.RemoveCropRegionShape()
-        if crop.Split:
-            crop.RemoveSplit()
-        for sb in scopeboxlist:
-            count += 1
-            print(str(count) + " " + vw.Name + " " + sb.Name)
-            try:
-                newView = revit.doc.GetElement(vw.Duplicate(dupop))
-                newView.Name = vw.Name + " " + sb.Name
-                sbParam = newView.LookupParameter("Scope Box")
-                sbParam.Set(sb.Id)
-                #newView.CropBoxActive = True
-            except Exception as duplerr:
-                logger.error('Error duplicating view "{}_{}" | {}'
-                             .format(revit.query.get_name(vw),revit.query.get_name(sb), duplerr))
-
-
-view = doc.ActiveView
-#selected_views = forms.select_views(filterfunc=duplicableview)
-
-selection = revit.get_selection()
-##selection_filter = \
-##            [x for x in revit.query.get_types_by_class(DB.GraphicsStyle,
-##                                                       doc=revit.doc)
-##             if x.GraphicsStyleCategory
-##             and x.GraphicsStyleCategory.CategoryType != DB.CategoryType.Internal
-##             and x.GraphicsStyleCategory.Name == "Scope Boxes"]
-##selection.set_to(list(selection_filter))
-
-dependant_views(view, selection)
-
+class ControlNetManagerWindow(forms.WPFWindow):    def __init__(self, xaml_file_name, reset_config=False):        forms.WPFWindow.__init__(self, xaml_file_name)        uri = Uri(tmp_file)        self.img.Source = BitmapImage(uri)        def ok_button(self, sender, args):        self.Close()    def window_closing(self, sender, args):        passtry:    ControlNetManagerWindow(xaml_file_name='ControlNetWindow.xaml').show_dialog()except Exception as kmex:    print(str(kmex))#os.remove(tmp_file)#print(list_data_files("jpg"))appdata.cleanup_appdata_folder()
